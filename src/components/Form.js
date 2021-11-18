acd4a6f8c4a56045c6aeac53d9d34833f14a3f73
Yurij Lishchuk <41346863+YurijLishchuk@users.noreply.github.com>
2021-11-18 23:14:02 +0200

    Add files via upload

---
+++ src/components/Form.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Form = ({ setInputText, todos, setTodos, inputText, setStatus }) => {
+    const inputTextHandler = (e) => {
+        setInputText(e.target.value);
+    }
+    const submitTodoHandler = (e) => {
+        e.preventDefault();
+        setTodos([
+            ...todos, {text: inputText, completed: false, id: Math.random() * 1000}
+        ]);
+        setInputText("");
+    }
+    const statusHandler  = (e) => {
+      setStatus(e.target.value);
+    }
+    return(
+        <form>
+        <input value={inputText} onChange ={inputTextHandler} type="text" className="todo-input" />
+        <button onClick={submitTodoHandler} className="todo-button" type="submit">
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <div className="select">
+          <select onChange={statusHandler} name="todos" className="filter-todo">
+            <option value="all">Всі</option>
+            <option value="completed">Виконано</option>
+            <option value="uncompleted">Не виконано</option>
+          </select>
+        </div>
+      </form>
+    );
+}
+
+export default Form;
